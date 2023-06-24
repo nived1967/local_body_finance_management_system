cf752d39039365d0b9a9e3700506f5d19de12233
Hareesh Mohan S <hareeshmohan10@gmail.com>
2023-06-24 18:50:02 +0530

    Latest updates

--- src/pages/authentication/user.js
+++ src/pages/authentication/user.js
@@ -1,10 +1,119 @@
-import React, { useState } from 'react';
-import Cookies from 'js-cookie';
-import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import Web3 from "web3";
+
+const contractABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "getOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "username",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "password",
+        type: "string",
+      },
+    ],
+    name: "login",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "username",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "password",
+        type: "string",
+      },
+    ],
+    name: "register",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "users",
+    outputs: [
+      {
+        internalType: "string",
+        name: "username",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "password",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+]; // Replace with your contract's ABI
+const contractAddress = "0xcc272ce9Fd4f7727ACcFbC21E58546B073e87a2A";
 
 const User = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [web3, setWeb3] = useState(null);
+  const [contract, setContract] = useState(null);
+  useEffect(() => {
+    const loadWeb3 = async () => {
+      if (window.ethereum) {
+        await window.ethereum.enable();
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
+        const contractInstance = new web3Instance.eth.Contract(
+          contractABI,
+          contractAddress
+        );
+        setContract(contractInstance);
+      } else {
+        console.error("Please install MetaMask to use this application.");
+      }
+    };
+
+    loadWeb3();
+  }, []);
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -14,18 +123,37 @@ const User = () => {
     setPassword(event.target.value);
   };
   const navigate = useNavigate();
-  const handleUserType=()=>{
-    Cookies.set("userType","admin",{ expires: 30, path: '/' })
-    navigate('/login/admin');
-  }
+  const handleUserType = () => {
+    Cookies.set("userType", "admin", { expires: 30, path: "/" });
+    navigate("/login/admin");
+  };
 
-  const handleLogin = () => {
+  const handleLogin = async (event) => {
     // Add your login logic here
-    Cookies.set("username",username,{ expires: 30, path: '/' })
-    Cookies.set("userType","User Logged In",{ expires: 30, path: '/' })
-    console.log('Username:', username);
-    console.log('Password:', password);
-    navigate('/user')
+    event.preventDefault();
+    console.log("contract", contract);
+    if (!contract) {
+      console.error("Contract not loaded");
+      return;
+    }
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      console.log("login username", username);
+      console.log("login password", password);
+      const loggedIn = await contract.methods
+        .login(username, password)
+        .call({ from: accounts[0] });
+      console.log("Logged in:", loggedIn);
+    } catch (error) {
+      console.error("Failed to log in", error);
+    }
+
+    Cookies.set("username", username, { expires: 30, path: "/" });
+    Cookies.set("userType", "User Logged In", { expires: 30, path: "/" });
+    console.log("Username:", username);
+    console.log("Password:", password);
+    navigate("/user");
   };
 
   return (
@@ -33,7 +161,10 @@ const User = () => {
       <h1 className="text-3xl font-bold mb-6">User Login</h1>
       <div className="bg-white shadow p-6 rounded-lg w-80">
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
             Username
           </label>
           <input
@@ -46,7 +177,10 @@ const User = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
             Password
           </label>
           <input
@@ -66,22 +200,22 @@ const User = () => {
         </button>
       </div>
       <div className="mt-4">
-      If you are an admin,
-      <button
-      className="bg-blue-100 hover:bg-blue-400 text-black font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-      onClick={handleUserType}
-    >
-      Click Here
-    </button>
+        If you are an admin,
+        <button
+          className="bg-blue-100 hover:bg-blue-400 text-black font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+          onClick={handleUserType}
+        >
+          Click Here
+        </button>
       </div>
       <div className="mt-4">
-      Click here to register,
-      <button
-      className="bg-blue-100 hover:bg-blue-400 text-black font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-      onClick={()=>navigate('/register/user')}
-    >
-      Register
-    </button>
+        Click here to register,
+        <button
+          className="bg-blue-100 hover:bg-blue-400 text-black font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+          onClick={() => navigate("/register/user")}
+        >
+          Register
+        </button>
       </div>
     </div>
   );
--- src/pages/authentication/userReg.js
+++ src/pages/authentication/userReg.js
@@ -1,14 +1,150 @@
-import React from 'react';
-import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Web3 from "web3";
+
+const contractABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "getOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "username",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "password",
+        type: "string",
+      },
+    ],
+    name: "login",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "username",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "password",
+        type: "string",
+      },
+    ],
+    name: "register",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "users",
+    outputs: [
+      {
+        internalType: "string",
+        name: "username",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "password",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+]; // Replace with your contract's ABI
+const contractAddress = "0xcc272ce9Fd4f7727ACcFbC21E58546B073e87a2A";
 
 const UserReg = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [web3, setWeb3] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  useEffect(() => {
+    const loadWeb3 = async () => {
+      if (window.ethereum) {
+        await window.ethereum.enable();
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
+        const contractInstance = new web3Instance.eth.Contract(
+          contractABI,
+          contractAddress
+        );
+        setContract(contractInstance);
+      } else {
+        console.error("Please install MetaMask to use this application.");
+      }
+    };
+
+    loadWeb3();
+  }, []);
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    if (!contract) {
+      console.error("Contract not loaded");
+      return;
+    }
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await contract.methods
+        .register(username, password)
+        .send({ from: accounts[0] });
+      console.log("User registered successfully");
+    } catch (error) {
+      console.error("Failed to register user", error);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-3xl font-bold mb-6">User Registration</h1>
       <div className="bg-white shadow p-6 rounded-lg w-80">
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
             Email
           </label>
           <input
@@ -16,10 +152,15 @@ const UserReg = () => {
             id="email"
             type="email"
             placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
             Username
           </label>
           <input
@@ -27,10 +168,15 @@ const UserReg = () => {
             id="username"
             type="text"
             placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
             Password
           </label>
           <input
@@ -38,26 +184,29 @@ const UserReg = () => {
             id="password"
             type="password"
             placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
+          onClick={handleRegister}
         >
           Register
         </button>
         <div className="mt-4">
-      Click here to login,
-      <button
-      className="bg-blue-100 hover:bg-blue-400 text-black font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-      onClick={()=>navigate('/login/user')}
-    >
-      Login
-    </button>
-      </div>
+          Click here to login,
+          <button
+            className="bg-blue-100 hover:bg-blue-400 text-black font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => navigate("/login/user")}
+          >
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default UserReg;
